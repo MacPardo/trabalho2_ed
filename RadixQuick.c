23e07b9b15e3bb8329a32d36ab03787fcef7282d
Marcos Theophilo Gobbi Adamczuk <marcos.adamczuk@gmail.com>
2016-12-10 01:00:47 -0200

    4 functions left

--- RadixQuick.c
+++ RadixQuick.c
@@ -8,10 +8,10 @@
 #define ASCII 256
 
 typedef struct _contato{
-	char nome[40];
-	char fone[30];
-	struct _contato *ant;
-	struct _contato *prox;
+  char nome[40];
+  char fone[30];
+  struct _contato *ant;
+  struct _contato *prox;
 } TpContato;
 
 void criarVetor(TpContato v[], int tam);
@@ -26,330 +26,385 @@ void quickSortLista(TpContato *lista);
 void radixSortLista(TpContato *a, int N);
 
 int main() {
-	int qtde, opcao = 0;
-	TpContato *lista, *lista_op, vetor[TAM_VETOR], vetor_op[TAM_VETOR];
-	clock_t start, stop;
+  int qtde, opcao = 0;
+  TpContato *lista, *lista_op, vetor[TAM_VETOR], vetor_op[TAM_VETOR];
+  clock_t start, stop;
 
-	lista = lista_op = NULL;
+  lista = lista_op = NULL;
 
-	while (opcao != 5) {
-		printf("Menu:\n1 - Criar lista\n2 - Criar vetor\n3 - Radix sort\n4 - Quick sort\n5 - Sair\n");
-		scanf("%d", &opcao);
-		getchar();
+  while (opcao != 5) {
+    printf("Menu:\n1 - Criar lista\n2 - Criar vetor\n3 - Radix sort\n4 - Quick sort\n5 - Sair\n");
+    scanf("%d", &opcao);
+    getchar();
 
-		switch (opcao) {
-			case 1:
-				printf("Criar lista\n");
-				printf("Quantos itens a lista terá?\n");
-				if (scanf("%d", &qtde)) {
-					getchar();
-					criarLista(&lista, qtde);
-					imprimirLista(lista);
-				} else {
-					printf("Falha ao obter a quantidade de itens.\n");
-				}
-				break;
-			case 2:
-				printf("Criar vetor\n");
-				criarVetor(vetor, TAM_VETOR);
-				imprimirVetor(vetor, TAM_VETOR);
-				break;
-			case 3:
-				printf("Radix sort\n");
-				copiarVetor(vetor, vetor_op, TAM_VETOR);
-				copiarLista(&lista, &lista_op);
-				// usar lista_op e vetor_op nas operações para manter os originais intactos
-				start = clock();
-				radixSortVetor(vetor_op, TAM_VETOR);
-				stop = clock();
-				imprimirVetor(vetor_op, TAM_VETOR);
-				printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
-				start = clock();
-				radixSortLista(lista_op, qtde);
-				stop = clock();
-				imprimirLista(lista_op);
-				printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
-				break;
-			case 4:
-				printf("Quick sort\n");
-				copiarVetor(vetor, vetor_op, TAM_VETOR);
-				copiarLista(&lista, &lista_op);
-				// usar lista_op e vetor_op nas operações para manter os originais intactos
-				start = clock();
-				quickSortVetor(vetor_op, 0, TAM_VETOR-1);
-				stop = clock();
-				imprimirVetor(vetor_op, TAM_VETOR);
-				printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
-				start = clock();
-				quickSortLista(lista_op);
-				stop = clock();
-				imprimirLista(lista_op);
-				printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
-				break;
-			case 5:
-				printf("Saindo...\n");
-				break;
-			default:
-				printf("Opção inválida!\n");
-		}
-	}
+    switch (opcao) {
+    case 1:
+      printf("Criar lista\n");
+      printf("Quantos itens a lista terá?\n");
+      if (scanf("%d", &qtde)) {
+	getchar();
+	criarLista(&lista, qtde);
+	imprimirLista(lista);
+      } else {
+	printf("Falha ao obter a quantidade de itens.\n");
+      }
+      break;
+    case 2:
+      printf("Criar vetor\n");
+      criarVetor(vetor, TAM_VETOR);
+      imprimirVetor(vetor, TAM_VETOR);
+      break;
+    case 3:
+      printf("Radix sort\n");
+      copiarVetor(vetor, vetor_op, TAM_VETOR);
+      copiarLista(&lista, &lista_op);
+      // usar lista_op e vetor_op nas operações para manter os originais intactos
+      start = clock();
+      radixSortVetor(vetor_op, TAM_VETOR);
+      stop = clock();
+      imprimirVetor(vetor_op, TAM_VETOR);
+      printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
+      start = clock();
+      radixSortLista(lista_op, qtde);
+      stop = clock();
+      imprimirLista(lista_op);
+      printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
+      break;
+    case 4:
+      printf("Quick sort\n");
+      copiarVetor(vetor, vetor_op, TAM_VETOR);
+      copiarLista(&lista, &lista_op);
+      // usar lista_op e vetor_op nas operações para manter os originais intactos
+      start = clock();
+      quickSortVetor(vetor_op, 0, TAM_VETOR-1);
+      stop = clock();
+      imprimirVetor(vetor_op, TAM_VETOR);
+      printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
+      start = clock();
+      quickSortLista(lista_op);
+      stop = clock();
+      imprimirLista(lista_op);
+      printf("Tempo: %lf segs.\n", ((double)(stop - start) / CLOCKS_PER_SEC));
+      break;
+    case 5:
+      printf("Saindo...\n");
+      break;
+    default:
+      printf("Opção inválida!\n");
+    }
+  }
 
-	return 0;
+  return 0;
 }
 
 int contaDigitos(int num) {
-    int n = 0;
-    if (num == 0) return 1;
+  int n = 0;
+  if (num == 0) return 1;
 
-    while(num) {
-        num /= 10;
-        n++;
-    }
+  while(num) {
+    num /= 10;
+    n++;
+  }
 
-    return n;
+  return n;
 }
 
 void criarVetor(TpContato v[], int tam) {
-	int i, num, digitos;
+  int i, num, digitos;
 
-	srand(time(NULL));
+  srand(time(NULL));
 
-	digitos = contaDigitos(tam);
+  digitos = contaDigitos(tam);
 
-	for (i = 0; i < tam; i++) {
-		num = rand() % tam;
-		snprintf(v[i].nome, sizeof v[i].nome, "Fulano %0*d", digitos, num);
+  for (i = 0; i < tam; i++) {
+    num = rand() % tam;
+    snprintf(v[i].nome, sizeof v[i].nome, "Fulano %0*d", digitos, num);
 
-		num = rand() % (MAX_FONE - MIN_FONE + 1) + MIN_FONE;
-		snprintf(v[i].fone, sizeof v[i].fone, "%d", num);
-	}
+    num = rand() % (MAX_FONE - MIN_FONE + 1) + MIN_FONE;
+    snprintf(v[i].fone, sizeof v[i].fone, "%d", num);
+  }
 }
 
 void copiarVetor(TpContato vo[], TpContato vd[], int tam) {
-	int i;
+  int i;
 
-	for (i = 0; i < tam; i++) {
-		vd[i] = vo[i];
-	}
+  for (i = 0; i < tam; i++) {
+    vd[i] = vo[i];
+  }
 }
 
 void imprimirVetor(TpContato v[], int tam) {
-	int i;
 
-	/*
-	completar
-	*/
+  /*
+    completar
+  */
+
+  printf("--------------------\n");
+  for(int i = 0; i < tam; i++){
+    printf("\nNome: %s\n", v[i].nome);
+    printf("\nFone: %s\n", v[i].fone);
+    printf("\n--------------------\n");
+  }
 }
 
+void swap(TpContato * a, TpContato * b){//função criada pelo aluno #ok
+  //troca os valores de a e b 
+  TpContato aux;
+  strcpy(aux.nome, a->nome);
+  strcpy(aux.fone, a->fone);
+
+  strcpy(a->nome, b->nome);
+  strcpy(a->fone, b->fone);
+
+  strcpy(b->nome, aux.nome);
+  strcpy(b->fone, aux.fone);
+}
+
+int asciiSmaller(char str1[], char str2[]){//função criada pelo aluno #ok
+  //retorna 1 se str1 vem antes na ordem alfabética
+  //caso contrário retorna 0
+
+  int i1, i2;
+
+  for(i1 = i2 = 0; str1[i1] && str2[i2]; i1++, i2++){
+    if(str1[i1] < str2[i2]) return 1;
+    else if (str1[i1] > str2[i2]) return 0;
+    //se forem iguais continua o loop
+  }
+
+  //se forem iguais até o fim que tiver menor comprimento,
+  //o com maior comprimento é considerado à frente na
+  //ordem alfabética
+
+  return strlen(str1) < strlen(str2);
+}
+
+
 int partitionVetor(TpContato vetor[], int p, int q) {
-	int i, j;
-	TpContato pivo, aux;
+  int i, j;
+  TpContato pivo;
 
-	pivo = vetor[p];
-	i = p - 1;
-	j = q + 1;
+  pivo = vetor[q];
 
-	/*
-	completar
-	*/
-
-	return j;
+  /*
+    completar
+  */
+  for(i = p - 1, j = p; j < q; j++){
+    if(asciiSmaller(vetor[j].nome, pivo.nome)){
+      i++;
+      swap(&vetor[i], &vetor[j]);
+    }
+  }
+  swap(&vetor[i + 1], &vetor[q]);
+  return i + 1;
 }
 
 void quickSortVetor(TpContato vetor[], int ini, int fim) {
-	int part;
-
-	if (ini < fim) {
-		part = partitionVetor(vetor, ini, fim);
-		quickSortVetor(vetor, ini, part);
-		quickSortVetor(vetor, part+1, fim);
-	}
+  int part;
+  
+  if (ini < fim) {
+    part = partitionVetor(vetor, ini, fim);
+    quickSortVetor(vetor, ini, part - 1);
+    quickSortVetor(vetor, part+1, fim);
+  }
 }
 
 int charAt(char vetor[], int pos) {
-	if (pos < strlen(vetor))
-    	return (int) vetor[pos];
-    return -1;
+  if (pos < strlen(vetor))
+    return (int) vetor[pos];
+  return -1;
 }
 
 void _radixSortVetor(TpContato a[], TpContato temp[], int lo, int hi, int d, int N) {
-	int i, r, count[ASCII+2] = {0};
+  int i, r, count[ASCII+2] = {0};
 
-	if (hi <= lo) return;
+  if (hi <= lo) return;
 
-	/*
-	completar
-	*/
+  /*
+    completar
+  */
 
-	for (r = 0; r < ASCII; r++) {
-		_radixSortVetor(a, temp, lo + count[r], lo + count[r+1] - 1, d+1, N);
-	}
+  for (r = 0; r < ASCII; r++) {
+    _radixSortVetor(a, temp, lo + count[r], lo + count[r+1] - 1, d+1, N);
+  }
 }
 
 void radixSortVetor(TpContato a[], int N) {
-    TpContato temp[N];
+  TpContato temp[N];
 
-    _radixSortVetor(a, temp, 0, N - 1, 0, N);
+  _radixSortVetor(a, temp, 0, N - 1, 0, N);
 }
 
 void criarLista(TpContato **lista, int tam) {
-	int i, num, digitos, tam_max_fone = MAX_FONE;
-	TpContato *temp, *ult = NULL;
+  int i, num, digitos, tam_max_fone = MAX_FONE;
+  TpContato *temp, *ult = NULL;
 
-	srand(time(NULL));
+  srand(time(NULL));
 
-	digitos = contaDigitos(tam);
+  digitos = contaDigitos(tam);
 
-	// exceção caso seja um numero muito grande de elementos, aumenta o tamanho do telefone
-	if (tam > (MAX_FONE - MIN_FONE))
-		tam_max_fone = MIN_FONE + tam;
+  // exceção caso seja um numero muito grande de elementos, aumenta o tamanho do telefone
+  if (tam > (MAX_FONE - MIN_FONE))
+    tam_max_fone = MIN_FONE + tam;
 
-	for (i = 0; i < tam; i++) {
-		temp = (TpContato *) malloc(sizeof(TpContato));
+  for (i = 0; i < tam; i++) {
+    temp = (TpContato *) malloc(sizeof(TpContato));
 
-		if (temp != NULL) {
-			num = rand() % tam;
-			snprintf(temp->nome, sizeof temp->nome, "Fulano %0*d", digitos, num);
+    if (temp != NULL) {
+      num = rand() % tam;
+      snprintf(temp->nome, sizeof temp->nome, "Fulano %0*d", digitos, num);
 
-			num = rand() % (tam_max_fone - MIN_FONE + 1) + MIN_FONE;
-			snprintf(temp->fone, sizeof temp->fone, "%d", num);
+      num = rand() % (tam_max_fone - MIN_FONE + 1) + MIN_FONE;
+      snprintf(temp->fone, sizeof temp->fone, "%d", num);
 
-			temp->ant = NULL;
-			temp->prox = NULL;
+      temp->ant = NULL;
+      temp->prox = NULL;
 
-			if (ult == NULL) {
-				*lista = ult = temp;
-			} else {
-				ult->prox = temp;
-				temp->ant = ult;
-				ult = temp;
-			}
+      if (ult == NULL) {
+	*lista = ult = temp;
+      } else {
+	ult->prox = temp;
+	temp->ant = ult;
+	ult = temp;
+      }
 
-		} else {
-			// erro de alocação
-			return;
-		}
-	}
+    } else {
+      // erro de alocação
+      return;
+    }
+  }
 }
 
 void copiarLista(TpContato **lista_o, TpContato **lista_d) {
-	TpContato *temp, *ult = NULL, *temp_o = *lista_o, *temp_d = *lista_d;
+  TpContato *temp, *ult = NULL, *temp_o = *lista_o, *temp_d = *lista_d;
 
-	while (temp_o != NULL) {
-		// se a lista destino já está alocada
-		if (temp_d != NULL) {
-			strcpy(temp_d->nome, temp_o->nome);
-			strcpy(temp_d->fone, temp_o->fone);
-			temp_o = temp_o->prox;
-			temp_d = temp_d->prox;
-		} else {
-			temp = (TpContato *) malloc(sizeof(TpContato));
+  while (temp_o != NULL) {
+    // se a lista destino já está alocada
+    if (temp_d != NULL) {
+      strcpy(temp_d->nome, temp_o->nome);
+      strcpy(temp_d->fone, temp_o->fone);
+      temp_o = temp_o->prox;
+      temp_d = temp_d->prox;
+    } else {
+      temp = (TpContato *) malloc(sizeof(TpContato));
 
-			if (temp != NULL) {
-				strcpy(temp->nome, temp_o->nome);
-				strcpy(temp->fone, temp_o->fone);
+      if (temp != NULL) {
+	strcpy(temp->nome, temp_o->nome);
+	strcpy(temp->fone, temp_o->fone);
 
-				temp->ant = NULL;
-				temp->prox = NULL;
+	temp->ant = NULL;
+	temp->prox = NULL;
 
-				if (ult == NULL) {
-					*lista_d = ult = temp;
-				} else {
-					ult->prox = temp;
-					temp->ant = ult;
-					ult = temp;
-				}
-
-				temp_o = temp_o->prox;
-			} else {
-				// erro de alocação
-				return;
-			}
-		}
+	if (ult == NULL) {
+	  *lista_d = ult = temp;
+	} else {
+	  ult->prox = temp;
+	  temp->ant = ult;
+	  ult = temp;
 	}
+
+	temp_o = temp_o->prox;
+      } else {
+	// erro de alocação
+	return;
+      }
+    }
+  }
 }
 
 void imprimirLista(TpContato *lista) {
-	TpContato *teste = lista;
+  TpContato * teste;
 
-	/*
-	completar
-	*/
+  /*
+    completar
+  */
+  printf("\n--------------------\n");
+  for(teste = lista; teste != NULL; teste = teste->prox){
+    printf("\nNome: %s\n", teste->nome);
+    printf("\nFone: %s\n", teste->fone);
+    printf("\n--------------------\n");
+  }
 }
 
 TpContato * fimDaLista(TpContato *lista) {
-	TpContato *fim = lista;
+  TpContato *fim = lista;
 
-	while (fim && fim->prox)
-		fim = fim->prox;
+  while (fim && fim->prox)
+    fim = fim->prox;
 
-	return fim;
+  return fim;
 }
 
 TpContato * partitionLista(TpContato *p, TpContato *q) {
-	TpContato *i, *j, *pivo, aux;
+  TpContato * i, * j, * pivo;
+  
+  pivo = p;
 
-	pivo = p;
-	i = p;
-	j = q;
+  //while(j != NULL && strcmp(j->nome, pivo->nome) > 0) {j = j->ant;}
+  //while(i != NULL && strcmp(i->nome, pivo->nome) < 0) {i = i->prox;}
 
-	while(j != NULL && strcmp(j->nome, pivo->nome) > 0) {j = j->ant;}
-	while(i != NULL && strcmp(i->nome, pivo->nome) < 0) {i = i->prox;}
+  /*
+    completar
+  */
 
-	/*
-	completar
-	*/
-
-	return j;
+  for(i = NULL, j = p; i != p && j != p; j = j->prox){
+    if(asciiSmaller(j->nome, pivo->nome)){
+      i = i == NULL ? p : i->prox;
+      swap(i, j);
+    }
+  }
+  
+  swap(i == NULL ? p : i->prox, q);
+  return i->prox;
 }
 
 void _quickSortLista(TpContato *ini, TpContato *fim) {
-	TpContato *part;
+  TpContato *part;
 
-	/*
-	completar
-	*/
+  /*
+    completar
+  */
 }
 
 void quickSortLista(TpContato *lista) {
-	TpContato *fim = fimDaLista(lista);
+  TpContato *fim = fimDaLista(lista);
 
-	_quickSortLista(lista, fim);
+  _quickSortLista(lista, fim);
 }
 
 void indexLista(TpContato *lista, TpContato *v[], int N) {
-	int i;	
-	TpContato *elem = lista;
+  int i;	
+  TpContato *elem = lista;
 
-	for (i = 0; i < N; elem = elem->prox, i++) {
-		v[i] = elem;			
-	}
+  for (i = 0; i < N; elem = elem->prox, i++) {
+    v[i] = elem;			
+  }
 }
 
 void copyData(TpContato *destino, TpContato *origem) {
-	if (origem == NULL) return;
+  if (origem == NULL) return;
 
-	strcpy(destino->nome, origem->nome);
-	strcpy(destino->fone, origem->fone);
+  strcpy(destino->nome, origem->nome);
+  strcpy(destino->fone, origem->fone);
 }
 
 void _radixSortLista(TpContato **a, TpContato **temp, int lo, int hi, int d, int N) {
-	TpContato *aux_a, *aux_temp;
-	int i, r, count[ASCII+2] = {0};
+  TpContato *aux_a, *aux_temp;
+  int i, r, count[ASCII+2] = {0};
 
-	if (hi <= lo) return;
+  if (hi <= lo) return;
 
-	/*
-	completar
-	*/
+  /*
+    completar
+  */
 }
 
 void radixSortLista(TpContato *a, int N) {
-	TpContato *temp, *index_a[N], *index_temp[N];
-	criarLista(&temp, N);
+  TpContato *temp, *index_a[N], *index_temp[N];
+  criarLista(&temp, N);
 	
-	indexLista(a, index_a, N);
-	indexLista(temp, index_temp, N);
+  indexLista(a, index_a, N);
+  indexLista(temp, index_temp, N);
 
-	_radixSortLista(index_a, index_temp, 0, N - 1, 0, N);
+  _radixSortLista(index_a, index_temp, 0, N - 1, 0, N);
 }
